ab35de55787ba2db1cdc55283ab8d1a8f29c7c70
Oluwakayode Momoh <68966387+lifestyle-sc@users.noreply.github.com>
2022-07-08 08:33:19 +0100

    removing the id field from  both the league and leagueMember model

--- model/league.model.js
+++ model/league.model.js
@@ -5,12 +5,6 @@ class League extends Model {}
 
 League.init(
     {
-        id:{
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
         name: {
             type: DataTypes.STRING,
             allowNull: false
--- model/leagueMemeber.model.js
+++ model/leagueMemeber.model.js
@@ -4,12 +4,6 @@ const sequelize = require('../config/db');
 class LeagueMember extends Model {};
 
 LeagueMember.init({
-    id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-        allowNull: false
-    },
     league_id: {
         type: DataTypes.INTEGER,
         allowNull: false
