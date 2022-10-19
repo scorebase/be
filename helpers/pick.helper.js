a6c6f455491f23dc3bba378677c65395c9762ac5
Olamide Aboyeji <aboyejiolamide15@gmail.com>
2022-10-19 22:19:27 +0100

    updated pick calculation

--- helpers/pick.helper.js
+++ helpers/pick.helper.js
@@ -11,28 +11,22 @@ module.exports.calculatePickResult = (home_pick, away_pick, home_result, away_re
     const pickDiff = home_pick - away_pick;
     let type = '';
 
-    if(home_pick === home_result && away_pick === away_result) {
-        points = POINTS_SYSTEM.exact;
-        type = 'exact';
-    } else if((resultDiff * pickDiff) <= 0 && (resultDiff !== pickDiff)) {
+    if((resultDiff * pickDiff) <= 0 && (resultDiff !== pickDiff)) {
         points = POINTS_SYSTEM.wrong;
     } else {
-        points = POINTS_SYSTEM.result;
-        type = 'result';
-
-        if(pickDiff === 0 && resultDiff === 0) {
-            if(Math.abs((home_result + away_result) - (home_pick + away_pick)) === 2) {
-                points = POINTS_SYSTEM.close;
-                type = 'close';
-            }
+        const closeIndex = Math.abs(
+            Math.abs(home_result - away_result) - Math.abs(home_pick - away_pick)
+        ) +
+            Math.abs(home_pick + away_pick - home_result - away_result) / 2;
+        if(closeIndex === 0) {
+            points = POINTS_SYSTEM.exact;
+            type = 'exact';
+        } else if(closeIndex <= 1.5) {
+            points = POINTS_SYSTEM.close;
+            type = 'close';
         } else {
-            if((home_pick === home_result && (Math.abs(away_pick - away_result) === 1))) {
-                points = POINTS_SYSTEM.close;
-                type = 'close';
-            } else if((away_pick === away_result && (Math.abs(home_pick - home_result) === 1))) {
-                points = POINTS_SYSTEM.close;
-                type = 'close';
-            }
+            points = POINTS_SYSTEM.result;
+            type = 'result';
         }
     }
 
