08b4f8d5f44f44f5c6527462baf2b1987144456e
Olamide Aboyeji <aboyejiolamide15@gmail.com>
2023-02-12 21:43:19 +0100

    Update constants.js

--- helpers/constants.js
+++ helpers/constants.js
@@ -3,7 +3,8 @@ module.exports.TOKEN_HEADER = 'x-user-token';
 module.exports.ADMIN_SECRET_HEADER = 'x-admin-secret';
 
 module.exports.TOKEN_TYPES = {
-    resetPassword: 'RESET_PASSWORD'
+    resetPassword: 'RESET_PASSWORD',
+    registerUser : 'REGISTER_USER'
 };
 
 module.exports.RESET_PASSWORD_TOKEN_LENGTH = 30;
@@ -13,8 +14,6 @@ module.exports.RESET_PASSWORD_EXP_TIME = 15;
 module.exports.ONE_MINUTE = 60000;
 
 module.exports.LEAGUE_TYPES = {general : 1, public : 2, private : 3};
-
-module.exports.TOKEN_TYPES = { registerUser : 'REGISTER_USER'};
 
 module.exports.REGISTER_USER_TOKEN_LENGTH = 6;
 
